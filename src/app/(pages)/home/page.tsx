48b2a868fe091de1eed8736c3849cf67451608bd
Edwilson <edwsilva@gmail.com>
2024-11-18 17:42:45 -0300

    ajuste conteúdo

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -8,49 +8,7 @@ import { FaSearch, FaSyncAlt } from "react-icons/fa";
 import CustomButton from "@/components/UI/Button/CustomButton";
 import ContainerWhiteBox from "@/components/Layout/Container/ContainerWhiteBox";
 
-type Section =
-  | "Dados Gerais"
-  | "Fornecedores"
-  | "Atas e Contratos"
-  | "Mapa de Pesquisa de Preços";
-
 export default function Home() {
-  const [activeSection, setActiveSection] = useState<Section>("Dados Gerais");
-
-  const sections: Record<Section, JSX.Element> = {
-    "Dados Gerais": (
-      <div className={styles.DadosGerais}>
-        <section className={styles.details}>
-          <TitleValue title="Processo" value="IPL-PRO-2024/0005999" />
-          <TitleValue title="Área Demandante" value="DAF" />
-        </section>
-
-        <section className={styles.object}>
-          <TitleValue
-            title="Objeto"
-            value="Contrato para a prestação de serviços de limpeza e conservação de [espaço/edifício], incluindo cronograma de serviços, equipe designada e critérios de avaliação da qualidade do serviço prestado."
-          />
-        </section>
-
-        <section className={styles.keywords}>
-          <p>Palavras Chaves</p>
-          <ul className={styles.keywordsList}>
-            <li className={styles.keywordItem}>Palavra1</li>
-            <li className={styles.keywordItem}>Palavra2</li>
-            <li className={styles.keywordItem}>Palavra3</li>
-            <li className={styles.keywordItem}>Palavra4</li>
-            <li className={styles.keywordItem}>Palavra5</li>
-          </ul>
-        </section>
-      </div>
-    ),
-    Fornecedores: <p>Conteúdo da seção Fornecedores</p>,
-    "Atas e Contratos": <p>Conteúdo da seção Atas e Contratos</p>,
-    "Mapa de Pesquisa de Preços": (
-      <p>Conteúdo da seção Mapa de Pesquisa de Preços</p>
-    ),
-  };
-
   const handleRefresh = () => {
     console.log("Refresh clicado!");
   };
