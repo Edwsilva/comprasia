8cdc7b717a7158bc18d69f16cf0a5b21b8a65acd
Edwilson <edwsilva@gmail.com>
2024-11-19 16:23:39 -0300

    Retirada do componente Invoices e mudança no nome do componente principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,54 +11,12 @@ import Invoices from "@/components/Layout/Invoices/Invoices";
 import ContainerHome from "@/components/Layout/Container/ContainerHome";
 // import style from "./page.module.css";
 import utilStyles from "@/utils/styles/utils.module.css";
-import Principal from "@/app/(pages)/home/page";
+import InitialPage from "@/app/(pages)/home/page";
 
 export default function Home() {
-  const invoices = [
-    {
-      id: 1,
-      createTs: "2024-11-10T12:00:00Z",
-      customer: {
-        name: "John Doe",
-        email: "john.doe@example.com",
-      },
-      status: "open",
-      value: 5000,
-    },
-    {
-      id: 2,
-      createTs: "2024-11-12T15:30:00Z",
-      customer: {
-        name: "Jane Smith",
-        email: "jane.smith@example.com",
-      },
-      status: "paid",
-      value: 12000,
-    },
-    {
-      id: 3,
-      createTs: "2024-11-14T10:00:00Z",
-      customer: {
-        name: "Alice Johnson",
-        email: "alice.johnson@example.com",
-      },
-      status: "void",
-      value: 7500,
-    },
-    {
-      id: 4,
-      createTs: "2024-11-15T14:00:00Z",
-      customer: {
-        name: "Bob Lee",
-        email: "bob.lee@example.com",
-      },
-      status: "uncollectible",
-      value: 3200,
-    },
-  ];
   return (
     // <ContainerHome>
-    <Principal />
+    <InitialPage />
     // </ContainerHome>
   );
 }
