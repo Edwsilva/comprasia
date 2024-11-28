c2ae448151c3dd50864da1263c4b866c64d77d17
Edwilson da Silva <edwilson.silva@rio.rj.gov.br>
2024-11-28 14:24:20 -0300

    configurações next.config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_KEYCLOAK_URL: process.env.NEXT_PUBLIC_KEYCLOAK_URL,
+    NEXT_PUBLIC_KEYCLOAK_REALM: process.env.NEXT_PUBLIC_KEYCLOAK_REALM,
+    NEXT_PUBLIC_KEYCLOAK_CLIENT_ID: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_ID,
+    NEXT_PUBLIC_KEYCLOAK_CLIENT_SECRET: process.env.NEXT_PUBLIC_KEYCLOAK_CLIENT_SECRET,
+    NEXT_PUBLIC_KEYCLOAK_REDIRECT_URI: process.env.NEXT_PUBLIC_KEYCLOAK_REDIRECT_URI,
+  },
 };
 
 export default nextConfig;
